0f5353102e0ca67fe83e277de07f500544361986
lZh1r <electroxhackerpig09@gmail.com>
2025-05-02 15:53:41 +0300

    gitignore + environmental variables for db connection

--- core/connect.py
+++ core/connect.py
@@ -1,10 +1,14 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = psycopg2.connect(
-    host="localhost",
-    user="postgres",
-    password="123456",
-    dbname="kanban",
-    port="5432",
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    dbname=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT"),
 )
 conn.autocommit = True
